8cf47d1250326257226da1db2e09678bc2472d58
Sudeep Juvekar <sjuvekar@gmail.com>
2026-02-22 22:10:49 -0500

    Add tiktoken files

--- llm-cpp/src/main.cpp
+++ llm-cpp/src/main.cpp
@@ -1,5 +1,12 @@
+#include "dataset.h"
+
+#include <tiktoken/encoding.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world";
+    auto gpt_dataset = llm::GPTDataset("hello world",
+        /*window_length=*/4,
+        /*strid*/1,
+        /*language_model=*/LanguageModel::R50K_BASE
+    );
 }
